b8a1911551e6a0a3c244a82aa676788f404fdc85
Badri <programmingwithbadri@outlook.com>
2022-12-23 12:10:54 +0530

    Link aws amplify

---
+++ src/services/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+    REGION: process.env.REGION,
+    USER_POOL_ID: process.env.USER_POOL,
+    APP_CLIENT_ID: process.env.APP_CLIENT,
+    IDENTITY_POOL_ID: process.env.IDENTITY_POOL,
+};
